5c7ce8d5e082aaf64377279512943d5ecaac5d99
raghu8 <rvmanthena@gmail.com>
2021-01-15 21:10:54 -0500

    Email notifications

---
+++ src/main/java/com/virtualbrick/emailnotification/EmailnotificationApplication.java
@@ -0,0 +1,13 @@
+package com.virtualbrick.emailnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailnotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailnotificationApplication.class, args);
+	}
+
+}
